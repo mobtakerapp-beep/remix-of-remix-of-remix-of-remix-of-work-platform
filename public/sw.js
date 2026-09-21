c74c1106bf57b55907d2f9213206936ca1827a76
mobtakerapp-beep <mobtakerapp@gmail.com>
2026-09-21 17:15:12 +0300

    Refresh PWA cache for new branded icon

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "student-guidance-v1";
-const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.ico"];
+const CACHE_NAME = "student-guidance-v2";
+const APP_SHELL = ["/", "/manifest.webmanifest", "/app-icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
